82bc763a7ca5f56472d3b3700eade562c42185af
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-05-27 23:43:14 -0300

    Ajustando Pessoa_Fisica.cs

--- Atividade/Pessoa_Fisica.cs
+++ Atividade/Pessoa_Fisica.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 
 namespace Atividade
 {
+    // Classe que representa uma pessoa física, herda de Clientes
     public class Pessoa_Fisica : Clientes
     {
+        // Propriedade para armazenar o CPF da pessoa física
         public string cpf { get; set; }
+        // Propriedade para armazenar o RG da pessoa física
         public string rg { get; set; }
     }
 }
